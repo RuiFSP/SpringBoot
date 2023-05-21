720840d74fd28c1dc3034346677f34389085b30a
ruipinto <ruifspinto@gmail.com>
2023-05-21 20:49:59 +0100

    my first commit

---
+++ src/main/java/com/ruifspinto/Main.java
@@ -0,0 +1,39 @@
+package com.ruifspinto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+
+        GreetResponse greetResponse = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Rui", 43, 57_000)
+        );
+
+        return greetResponse;
+    }
+
+    record Person(String name, int age, double savings) {
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {
+    }
+
+}
